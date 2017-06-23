09e6e142cd465c00e5d8f3f898539b5e8b8c88e1
焦康 <Administrator@HP201-1>
2017-06-23 17:26:40 +0800

    完成

--- 图书管理系统/Form1.Designer.cs
+++ 图书管理系统/Form1.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("C#语言");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("悬疑");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("所有书籍", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8});
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.listView2 = new System.Windows.Forms.ListView();
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -49,15 +59,21 @@
             this.panel2.Controls.Add(this.listView2);
             this.panel2.Location = new System.Drawing.Point(143, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(276, 441);
+            this.panel2.Size = new System.Drawing.Size(508, 441);
             this.panel2.TabIndex = 0;
             // 
             // listView2
             // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
             this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView2.Location = new System.Drawing.Point(0, 0);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(276, 441);
+            this.listView2.Size = new System.Drawing.Size(508, 441);
             this.listView2.TabIndex = 1;
             this.listView2.UseCompatibleStateImageBehavior = false;
             // 
@@ -66,14 +82,43 @@
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
+            treeNode7.Name = "节点1";
+            treeNode7.Text = "C#语言";
+            treeNode8.Name = "节点2";
+            treeNode8.Text = "悬疑";
+            treeNode9.Name = "节点0";
+            treeNode9.Text = "所有书籍";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode9});
             this.treeView1.Size = new System.Drawing.Size(137, 441);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Width = 100;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 440);
+            this.ClientSize = new System.Drawing.Size(648, 440);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -91,6 +136,11 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
 
--- 图书管理系统/Form1.cs
+++ 图书管理系统/Form1.cs
@@ -12,6 +12,10 @@ namespace 图书管理系统
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlconn = null;
+        string strconn = "Data Source=HP201-1;Initial Catalog=Book;Integrated Security=True";
+        //定义一个变量表示选中节点s
+        string category;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,66 @@ namespace 图书管理系统
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //将listview改为列表显式
+            listView2.View = View.Details;
+            BookList();
+        }
+        private void BookList()
+        {
+            //先清空listview中的内容
+            listView2.Items.Clear();
+            sqlconn = new SqlConnection(strconn);
+            try
+            {
+                sqlconn.Open();
+                string sql = string.Format("select BNo,BName,Author,Press,Price from Books where Category like '%{0}%' ",category);
+                SqlCommand cmd = new SqlCommand(sql,sqlconn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                //遍历dr
+                while (dr.Read())
+                {
+                    //获取查询的数据
+                    string bookno = dr["BNo"].ToString();
+                    string bookname = dr["BName"].ToString();
+                    string bookauthor = dr["Author"].ToString();
+                    string bookpress = dr["Press"].ToString();
+                    double price = Convert.ToDouble(dr["Price"]);
+                    //创建listviewitems
+                    ListViewItem item = new ListViewItem(bookno);
+                    //添加子项
+                    item.SubItems.Add(bookname);
+                    item.SubItems.Add(bookname);
+                    item.SubItems.Add(bookpress);
+                    item.SubItems.Add(Convert.ToString(price));
 
+                    listView2.Items.Add(item);
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlconn.Close();
+            }
+
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode.Level == 1)
+            {
+                category = treeView1.SelectedNode.Text;
+            }
+            else
+            {
+                category = null;
+            }
+            BookList();
         }
     }
 }
